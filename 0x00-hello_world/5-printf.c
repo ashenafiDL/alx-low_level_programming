4870c388d897de328367a17a83a7e1245b26cdfe
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-06-26 05:45:06 -0700

    solve problem 5. hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the programm
+ *
+ * Return: 0 if there are no problems
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
+
