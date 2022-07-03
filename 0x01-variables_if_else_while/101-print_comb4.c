1d52b5690562bc498cda0069a01ce97d9eabc56d
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-07-03 14:17:44 -0700

    add all advanced

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the prigram
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char i, j, k;
+
+	i = 48;
+	while (i <= 57)
+	{
+		j = i + 1;
+		while (j <= 57)
+		{
+			k = j + 1;
+			while (k <= 57)
+			{
+				putchar(i);
+				putchar(j);
+				putchar(k);
+
+				if (i != 55)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar(10);
+	return (0);
+}
