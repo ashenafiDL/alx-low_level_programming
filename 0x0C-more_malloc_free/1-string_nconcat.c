3ae97aea1cb0353b4fde8a42ac1c9426e60b6644
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-07-28 15:54:43 -0700

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,9 +29,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 
 	if (s1 == NULL)
-		s1 = '\0';
+		s1 = "";
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "";
 
 	size1 = _strlen(s1);
 	size2 = _strlen(s2);
