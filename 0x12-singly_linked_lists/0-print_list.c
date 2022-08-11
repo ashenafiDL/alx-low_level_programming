462ed374cd4f58a56bbad29f5546dd3db9c1dcd2
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-08-11 11:21:12 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,17 +14,12 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)");
-			count++;
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-			count++;
-			h = h->next;
-		}
-	}
 
+		count++;
+		h = h->next;
+	}
 	return (count);
 }
