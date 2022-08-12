ce110cc91d004c8bf5f651239d10dcc8bf2db825
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-08-12 00:42:05 -0700

    add task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * before_main - prints a text before the main function is called
+ */
+void __attribute__((constructor)) before_main()
+{
+	printf(
+	"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+	);
+}
