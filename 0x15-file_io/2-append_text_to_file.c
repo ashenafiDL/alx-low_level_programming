874e9f6d7a91efb907aa4c0e16ca87c2c65e915c
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-08-22 11:45:53 -0700

    remove trailing whitespace on line 32

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -29,7 +29,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	write_count = write(fd, text_content, i);
 	if (write_count == -1)
 		return (-1);
-	
+
 	close(fd);
 
 	return (1);
