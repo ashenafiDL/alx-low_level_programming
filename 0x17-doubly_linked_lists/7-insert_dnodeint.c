59222386165783965cebdad6712f0daec60c172a
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-09-30 04:18:42 +0300

    add mandatory task 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ *
+ * @h: the address of the first node
+ * @idx: the index of the list where the new node should be added
+ * @n: the new data to be inserted
+ *
+ * Return: the address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i;
+	dlistint_t *temp, *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	temp = *h;
+	i = 0;
+	while (temp != NULL && i != idx - 1)
+	{
+		i++;
+		temp = temp->next;
+	}
+
+	if (temp == NULL)
+		return (NULL);
+
+	new->next = temp->next;
+	new->prev = temp;
+	temp->next->prev = new;
+	temp->next = new;
+
+	return (new);
+}
