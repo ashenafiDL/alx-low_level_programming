e4c442b4b49b7f384d58d83851088475706a5d08
Ashenafi Debella <ashenafi.debella101@gmail.com>
2022-10-21 10:04:50 +0300

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,6 +8,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	int i;
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
@@ -15,5 +16,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
 	return (table);
 }
